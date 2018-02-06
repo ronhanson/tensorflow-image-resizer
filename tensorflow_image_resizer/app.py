cb8f85724f701472eb4ad9371441b27649de74fa
Ronan Delacroix <ronan.delacroix@gmail.com>
2018-02-06 01:24:45 +0100

    Upload button working + WIP upload server method doing resize of input images. Need Tensor Flow now.

--- tensorflow_image_resizer/app.py
+++ tensorflow_image_resizer/app.py
@@ -7,8 +7,14 @@ Tensor Flow Image Resizer
 :author: Ronan Delacroix
 """
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, flash, url_for
 from whitenoise import WhiteNoise
+from werkzeug.utils import secure_filename
+import tempfile
+import shutil
+import json
+import zipfile
+from PIL import Image
 
 
 # Flask setup
@@ -17,6 +23,24 @@ app = Flask('tensorflow-image-resizer',
             template_folder='templates')
 app.wsgi_app = WhiteNoise(app.wsgi_app, root='static/')
 app_name = "Tensor Flow Image Resizer"
+
+
+ALLOWED_EXTENSIONS = set(['bmp', 'png', 'jpg', 'jpeg', 'gif', 'tif', 'tiff'])
+
+SIZES = [
+    (2048, 2048),
+    (1024, 1024),
+    (512, 512),
+    (256, 256),
+    (128, 128),
+    (64, 64),
+    (32, 32),
+]
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 # Routes
@@ -30,4 +54,61 @@ def index():
         'app_name': app_name,
     }
     return render_template('index.html', **tmpl_dict)
+
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    """
+    Upload image
+    """
+    upload_folder = tempfile.mkdtemp()
+    try:
+        if request.method == 'POST':
+            # check if the post request has the file part
+            if 'file' not in request.files:
+                flash('No file part')
+                return redirect(request.url)
+            file = request.files['file']
+            # if user does not select file, browser also
+            # submit a empty part without filename
+            if file.filename == '':
+                flash('No selected file')
+                return redirect(request.url)
+            if file and allowed_file(file.filename):
+                filename = secure_filename(file.filename)
+                fullpath = os.path.join(upload_folder, filename)
+                file.save(fullpath)
+
+                src_img = Image.open(fullpath, 'r')
+
+                derivatives = []
+                for size in SIZES:
+                    new_filename = os.path.splitext(filename)[0]+'.' + '%d_%d' % size
+                    new_fullpath = os.path.join(upload_folder, new_filename)
+                    png_fullpath = new_fullpath + '.png'
+                    jpg_fullpath = new_fullpath + '.jpg'
+                    resized_img = src_img.copy()
+                    resized_img.thumbnail(size, Image.ANTIALIAS)
+                    resized_img.save(png_fullpath, "PNG")
+                    resized_img.save(jpg_fullpath, "JPEG")
+
+                    derivatives.append(png_fullpath)
+                    derivatives.append(jpg_fullpath)
+
+                    # call tensor flow here on derivatives
+
+                    #with zipfile.ZipFile('/tmp/images.zip', 'w') as z:
+                    #    for f in derivatives:
+                    #        z.write(f)
+
+                return json.dumps({'files': [
+                    {
+                        "name": filename,
+                        "type": file.content_type,
+                        "size": os.path.getsize(fullpath)
+                    }
+                ]})
+    finally:
+        shutil.rmtree(upload_folder)
+    return {}
 
--- tensorflow_image_resizer/static/js/upload.js
+++ tensorflow_image_resizer/static/js/upload.js
@@ -2,8 +2,99 @@
 @author Ronan Delacroix
 */
 
-$(document).ready(function() {
-
-    // ...
-
+$(function () {
+    'use strict';
+    // Change this to the location of your server-side upload handler:
+    var url = '/upload',
+        uploadButton = $('<button/>')
+            .addClass('upload-button')
+            .prop('disabled', true)
+            .text('Processing...')
+            .on('click', function () {
+                var $this = $(this),
+                    data = $this.data();
+                $this
+                    .off('click')
+                    .text('Abort')
+                    .on('click', function () {
+                        $this.remove();
+                        data.abort();
+                    });
+                data.submit().always(function () {
+                    $this.remove();
+                });
+            });
+    $('#fileupload').fileupload({
+        url: url,
+        dataType: 'json',
+        autoUpload: false,
+        acceptFileTypes: /(\.|\/)(gif|jpe?g|png|bmp)$/i,
+        maxFileSize: 99900000,
+        disableImageResize: true,
+        previewMaxWidth: 100,
+        previewMaxHeight: 100,
+        previewCrop: true
+    }).on('fileuploadadd', function (e, data) {
+        $("#image-upload > span.upload-button").addClass('done')
+        data.context = $('<div/>').appendTo('#files');
+        $.each(data.files, function (index, file) {
+            var node = $('<p/>')
+                    .append($('<span/>').text(file.name));
+            if (!index) {
+                node
+                    .append('<br>')
+                    .append(uploadButton.clone(true).data(data));
+            }
+            node.appendTo(data.context);
+        });
+    }).on('fileuploadprocessalways', function (e, data) {
+        var index = data.index,
+            file = data.files[index],
+            node = $(data.context.children()[index]);
+        if (file.preview) {
+            node
+                .prepend('<br>')
+                .prepend(file.preview);
+        }
+        if (file.error) {
+            node
+                .append('<br>')
+                .append($('<span class="text-danger"/>').text(file.error));
+        }
+        if (index + 1 === data.files.length) {
+            data.context.find('button')
+                .text('Upload')
+                .prop('disabled', !!data.files.error);
+        }
+    }).on('fileuploadprogressall', function (e, data) {
+        var progress = parseInt(data.loaded / data.total * 100, 10);
+        $('#progress .progress-bar').css(
+            'width',
+            progress + '%'
+        );
+    }).on('fileuploaddone', function (e, data) {
+        $.each(data.result.files, function (index, file) {
+            if (file.url) {
+                var link = $('<a>')
+                    .attr('target', '_blank')
+                    .prop('href', file.url);
+                $(data.context.children()[index])
+                    .wrap(link);
+            } else if (file.error) {
+                var error = $('<span class="text-danger"/>').text(file.error);
+                $(data.context.children()[index])
+                    .append('<br>')
+                    .append(error);
+            }
+        });
+    }).on('fileuploadfail', function (e, data) {
+        $.each(data.files, function (index) {
+            var error = $('<span class="text-danger"/>').text('File upload failed.');
+            $(data.context.children()[index])
+                .append('<br>')
+                .append(error);
+        });
+    }).prop('disabled', !$.support.fileInput)
+        .parent().addClass($.support.fileInput ? undefined : 'disabled');
 });
+
